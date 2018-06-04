b2f95648c77d7138427f43143e775069dd4707e4
Renée Kooi <renee@kooi.me>
2018-06-04 14:37:08 +0200

    Add ES module entry point for `uppy` package.

---
+++ packages/uppy/index.mjs
@@ -0,0 +1,16 @@
+import Core from '@uppy/core'
+import Dashboard from '@uppy/dashboard'
+import Informer from '@uppy/informer'
+import StatusBar from '@uppy/statusbar'
+import ThumbnailGenerator from '@uppy/thumbnail-generator'
+import Tus from '@uppy/tus'
+
+export default Core
+export {
+  Core,
+  Dashboard,
+  Informer,
+  StatusBar,
+  ThumbnailGenerator,
+  Tus
+}
